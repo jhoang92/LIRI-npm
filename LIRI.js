032b08b2c067dd75eaa790b74a9945c5a8752e33
jhoang92 <pergexplr@gmail.com>
2018-04-15 22:27:14 -0600

    working twitter added spotify

--- LIRI.js
+++ LIRI.js
@@ -2,50 +2,86 @@
 packages & keys
 *******************/
 require('dotenv').config();
-const fs = require('fs');
+var fs = require('fs');
 
-const keys = require('./keys.js');
-const request = require('request');
-const client = require('twitter');
-const spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var request = require('request');
+var client = require('twitter');
+var spotify = require('node-spotify-api');
 
-const clientKey = new client(keys.twitter);
-const spotifyKey = new spotify(keys.spotify);
+var clientKey = new client(keys.twitter);
+var spotifyKey = new spotify(keys.spotify);
+
+var command = process.argv[2];
+var value = process.argv.slice(3).join(' ');
 
 /*******************
-Targetting the line where we will type in commands
-*******************/
-const command = (process.argv.slice(2).join(function () {
+ Variables to call needed npmFunctions
+ *******************/
+var getTweets = () => {
+    if (command === 'my-tweets') {
+        console.log('gathering tweets...')
+        myTweets();
+    }
+};
+getTweets();
 
-    function myTweets() {
-        const params = { screen_name: 'Real_M_FreeMan' };
-        client.get('statuses/user_timeline.json', params, function (err, tweets, response) {
-            if (err) {
-                console.log(err)
-            };
-           
-        });
-    };
+var getSpotify = () => {
+    if (command === 'spotify-this-song') {
+        console.log('gathering spotify results...')
+        spotSearch();
+    }
+};
+spotSearch();
+this.omdb = () => {
+};
+
+this.txt = () => {
+};
+
+
+/*******************
+ functions to run npms
+ *******************/
+function myTweets() {
+    var params = { screen_name: 'Real_M_FreeMan', count: 20 };
+
+    clientKey.get('statuses/user_timeline.json', params, function (err, tweets, response) {
+
+        if (!err) {
+            console.log(tweets);
+        };
+
+    });
+};
+
+
+function spotSearch(value) {
+
+    if (!value) {
+        value = 'purple rain';
+    }
+    spotifyKey.search({ type: 'track', query: value }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        var songInfo = data.tracks.items;
+        console.log("Artist(s): " + songInfo[0].artists[0].name);
+        console.log("Song Name: " + songInfo[0].name);
+        console.log("Preview Link: " + songInfo[0].preview_url);
+        console.log("Album: " + songInfo[0].album.name);
+    });
 }
 
 //end of main command function
-)
-);
 
-fs.readFile('random.txt', 'utf8', function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
-});
-
-console.log(clientKey.constructor);
-console.log(spotifyKey.constructor)
 
 
 /*******************
 How to make this line accept these commands
-const command = process.argv.slice(2).join(' ');// * `my-tweets`
+var command = process.argv.slice(2).join(' ');// * `my-tweets`
 
 // * `spotify-this-song`
 
@@ -54,7 +90,7 @@ const command = process.argv.slice(2).join(' ');// * `my-tweets`
 // * `do-what-it-says`
 *******************/
 /****************
-const input = process.argv.slice(3).join(' ');
+var input = process.argv.slice(3).join(' ');
 should take these
 -song name- 
 -movie name-
